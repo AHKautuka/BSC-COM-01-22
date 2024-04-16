4f3f82ae31bdbf027c1145b6c5d35e6c2b7bdabe
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-04-16 13:49:12 +0200

    Added Printing of Subscription Notification

--- Exercise1.cpp
+++ Exercise1.cpp
@@ -2,9 +2,40 @@
 #include <cstdlib>
 using namespace std;
 
+void printSubscriptionNotification(unsigned int daysUntilExpiration);
+
 int main()
 {
 	unsigned int daysUntilExpiration = rand() % 12;
 	
+	printSubscriptionNotification(daysUntilExpiration);
+	cout << endl;
+	
 	return 0;
+}
+
+void printSubscriptionNotification(unsigned int daysUntilExpiration)
+{
+	if (daysUntilExpiration > 10)
+	{
+		cout << "You have an active subscription.\n";
+	}
+	else if (daysUntilExpiration == 0)
+	{
+		cout << "Your subscription has expired.\n";
+	}
+	else if (daysUntilExpiration <= 1)
+	{
+		cout << "Your subscription expires within a day!\n";
+		cout << "Renew now and save 20%!\n";
+	}
+	else if (daysUntilExpiration <= 5)
+	{
+		cout << "Your subscription expires in " << daysUntilExpiration << " days\n";
+		cout << "Renew now and save 10%!\n";
+	}
+	else
+	{
+		cout << "Your subscription will expire soon. Renew now!\n";
+	}
 }
