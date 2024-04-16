a344ada19ada2c8d6d2adb6fad5f0ce59e1a1a58
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-04-16 19:49:50 +0200

    Added checking and printing functionality

--- Exercise3.cpp
+++ Exercise3.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 int main()
 {
-	string array[] = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+	int arrayLength = 8;
+	string array[arrayLength] = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+	
+	for (int i = 0; i < arrayLength; i++)
+	{
+		if (array[i].front() == 'B')
+		{
+			cout << array[i] << "\n";
+		}
+	}
+	
+	cout << endl;
+	
 	return 0;
 }
