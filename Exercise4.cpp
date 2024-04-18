d3cf479c2df16b507e610f815dce823f8a894ab2
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-04-18 08:43:51 +0200

    Added bounds checking and relevant feedback

--- Exercise4.cpp
+++ Exercise4.cpp
@@ -2,10 +2,16 @@
 #include <string>
 using namespace std;
 
+bool isInRange(const int value, const int min, const int max);
+
 int main()
 {
+	int min = 5;
+	int max = 10;
 	bool running = true;
-	cout << "Enter an integer value between 5 and 10.\n";
+	
+	cout << "Enter an integer value between " << min << " and " << max << ".\n";
+	
 	while (running)
 	{
 		string input;
@@ -17,15 +23,29 @@ int main()
 		{
 			number = stoi(input);
 		}
-		catch(const std::exception& e)
+		catch(const exception& e)
 		{
 			cout << "Sorry, you have entered an invalid number, please try again.\n";
 			continue;
 		}
 		
-		cout << "You entered" << number << "\n";
+		if (isInRange(number, min, max))
+		{
+			cout << "Your input value(" << number << ") has been accepted.";
+			running = false;
+		}
+		else
+		{
+			cout << "You entered " << number << ". Please enter an number between " << min << " and " << max << ".\n";
+		}
 	}
-	cout << endl;
 	
+	cout << endl;
 	return 0;
+}
+
+
+bool isInRange(const int value, const int min, const int max)
+{
+	return value >= min && value <= max;
 }
