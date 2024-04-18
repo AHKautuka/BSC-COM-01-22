246f29f485e1c499bf3966620481f8fe8ff016c2
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-04-18 07:09:18 +0200

    Added repeated user prompting and input checking
    Added an infinite while loop with a boolean exit condition variable
    Added user prompting to infoem and retrieve the 'integer' entered
    Added integer conversion to attempt to use the input as an integer
    Added error checking on the integer conversion to notify of invalid input

--- Exercise4.cpp
+++ Exercise4.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
+	bool running = true;
+	cout << "Enter an integer value between 5 and 10.\n";
+	while (running)
+	{
+		string input;
+		getline(cin, input);
+		
+		int number;
+		
+		try
+		{
+			number = stoi(input);
+		}
+		catch(const std::exception& e)
+		{
+			cout << "Sorry, you have entered an invalid number, please try again.\n";
+			continue;
+		}
+		
+		cout << "You entered" << number << "\n";
+	}
+	cout << endl;
 	
 	return 0;
 }
