0c1beb639f417f0dd775932ef486c8e0841bf2c3
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-04-18 10:32:55 +0200

    Added prompt for selection of a shape or quitting

--- Exercise5.cpp
+++ Exercise5.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-void shapePrompt();
+void shapePrompt(vector<string> shapes);
 
 int main()
 {
-	shapePrompt();
+	vector<string> shapes = {"Square", "Rectangle", "Triangle"};
+	shapePrompt(shapes);
+	cout << endl;
 	return 0;
 }
 
-void shapePrompt()
+void shapePrompt(vector<string> shapes)
 {
+	cout << "Please select the area of the shape to calculate\n";
 	
+	for (int i = 0; i < shapes.size(); i++)
+	{
+		cout << (i + 1) << ". " << shapes[i] << "\n";
+	}
+	cout << shapes.size() << ". Quit Program\n";
+	
+	cout << endl;
+	cout << "Enter selection\n";
 }
