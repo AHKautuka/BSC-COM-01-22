bd93d93ead08b8b98192d562b8ad12acb1d15996
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-04-19 12:22:07 +0200

    Added word counting
    Added tokenize function to split strings into a list of tokens
    Added outputting of number of tokens
    Note that it is assumed that every non-empty token is a word

--- Exercise6.cpp
+++ Exercise6.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <sstream>
+#include <vector>
+#include <iterator>
 using namespace std;
 
 string readFile(const string& path);
 int countVowels(const string& text);
 bool isVowel(char character);
+int countWords(const string& text);
+vector<string> tokenize(const string& text);
 
 int main()
 {
 	string fileData = readFile("res/Exercise6_Info.txt");
 	
 	cout << "There are " << countVowels(fileData) << " vowels.\n";
+	cout << "There are " << countWords(fileData) << " words.\n";
 	
 	cout << endl;
 	
@@ -61,4 +66,27 @@ bool isVowel(char character)
 {
 	character = toupper(character);
 	return character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U';
+}
+
+int countWords(const string& text)
+{
+	int wordNum;
+	vector<string> tokens = tokenize(text);
+	
+	for(string token : tokens)
+	{
+		if(!token.empty())
+		{
+			wordNum++;
+		}
+	}
+	
+	return wordNum;
+}
+
+vector<string> tokenize(const string& text)
+{
+	istringstream textStream(text);
+	vector<string> tokens{istream_iterator<string>{textStream}, istream_iterator<string>{}};
+	return tokens;
 }
