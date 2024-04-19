9fb7ec850deb05f39f37e9b93866f65938817ae5
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-04-19 19:54:52 +0200

    Various updates to Exercise5
    Added actual selection feedback logic
    Updated vectors to contain shape pointers instead of strings
    Updated and overloaded invalid input function to take a string or int specifically
    Added range detection functionality

--- Exercise5.cpp
+++ Exercise5.cpp
@@ -6,37 +6,44 @@
 
 using namespace std;
 
-void shapePrompt(const vector<string>& shapes);
-void selectShape(const vector<string>& shapes);
-void printInvalidInput(const auto& input);
+void shapePrompt(const vector<Shape*>& shapes);
+void selectShape(const vector<Shape*>& shapes, bool& running);
+void printInvalidInput(const string& input);
+void printInvalidInput(const int& input);
+bool isInRange(const int value, const int min, const int max);
 
 int main()
 {
-	vector<string> shapes = {"Square", "Rectangle", "Triangle"};
+	vector<Shape*> shapes = {new Square, new Rectangle, new Triangle};
 	
 	bool running = true;
 	while(running){
 		shapePrompt(shapes);
-		selectShape(shapes);
+		selectShape(shapes, running);
 	}
 	cout << endl;
+	
+	for(Shape* shape : shapes){
+		delete shape;
+	}
+	
 	return 0;
 }
 
-void shapePrompt(const vector<string>& shapes)
+void shapePrompt(const vector<Shape*>& shapes)
 {
 	cout << "Please select the area of the shape to calculate\n";
 	
 	for (int i = 0; i < shapes.size(); i++)
 	{
-		cout << (i + 1) << ". " << shapes[i] << "\n";
+		cout << (i + 1) << ". " << shapes[i]->getName() << "\n";
 	}
 	cout << (shapes.size() + 1) << ". Quit Program\n";
 	
 	cout << endl;
 }
 
-void selectShape(const vector<string>& shapes){
+void selectShape(const vector<Shape*>& shapes, bool& running){
 	cout << "Enter selection\n";
 	string input;
 	getline(cin, input);
@@ -51,10 +58,35 @@ void selectShape(const vector<string>& shapes){
 		printInvalidInput(input);
 		return;
 	}
+	
+	if(selection == (shapes.size() + 1))
+	{
+		running = false;
+		return;
+	}
+	else if (isInRange(selection, 1, shapes.size()))
+	{
+		cout << shapes[selection - 1]->getName() << "\n";
+	}
+	else
+	{
+		printInvalidInput(selection);
+	}
 }
 
-void printInvalidInput(const auto& input)
+void printInvalidInput(const string& input)
 {
 	cout << "Your input was: " << input << " which is an invalid input\n";
 	cout << "Please enter a valid input!!!\n";
+}
+
+void printInvalidInput(const int& input)
+{
+	cout << "Your input was: " << input << " which is an invalid input\n";
+	cout << "Please enter a valid input!!!\n";
+}
+
+bool isInRange(const int value, const int min, const int max)
+{
+	return value >= min && value <= max;
 }
