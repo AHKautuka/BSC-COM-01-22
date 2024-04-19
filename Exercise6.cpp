f8cf4bb61dc6d975b4911d6161423c753e1decb1
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-04-19 09:50:23 +0200

    Added reading of text file, when possible
    Added message to notify any failure to open the text file
    Added the line-by-line reading of the text file
    Added the storing of text file contents into a single string
    Added closing of text file when done reading

--- Exercise6.cpp
+++ Exercise6.cpp
@@ -4,6 +4,25 @@ using namespace std;
 
 int main()
 {
+	string fileData = "";
+	ifstream textFile("res/Exercise6_Info.txt");
+	
+	if (textFile.is_open())
+	{
+		string currentLine;
+		while (getline(textFile, currentLine))
+		{
+			fileData.append(currentLine).append("\n");
+		}
+		
+		textFile.close();
+	}
+	else
+	{
+		cout << "Unable to open file\n";
+	}
+	
+	cout << endl;
 	
 	return 0;
 }
