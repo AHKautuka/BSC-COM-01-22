ab4d93a35128d776584fa3ac2059a50417b73172
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-04-21 12:04:20 +0200

    Added documentation to declared functions

--- Exercise6.cpp
+++ Exercise6.cpp
@@ -5,12 +5,51 @@
 #include <iterator>
 using namespace std;
 
+/* Reads a file as a string.
+Takes the [absolute or relative] path of the file as a string.
+Returns the contents of the file as a string, if the file is found. Otherwise, it returns an empty string.*/
 string readFile(const string& path);
-int countVowels(const string& text);
+
+/* Counts the number of English vowels (a, e, i, o, u) in a given text.
+Takes string (the text) to count vowels from.
+Returns the number of vowels*/
+unsigned int countVowels(const string& text);
+
+/* Checks if a character is an English vowel (a, e, i, o, u).
+Takes in a character.
+Returns true if the character is a vowel; returns false, otherwise.*/
 bool isVowel(char character);
+
+/* Counts the number of words in a given text.
+
+Take note that it is assumed that all non-empty tokens are words,
+and do not necessarily have to conform to any legal spelling or grammar.
+For Example, " I took COM221 in 2024 at age 19 " has 8 words.
+
+Takes a string (the text) to count words from.
+Returns the number of words.*/
 int countWords(const string& text);
+
+/* Splits a given text into a list of tokens.
+Take note that the resultant list may contain empty strings.
+Takes a string (the text) to tokenize.
+Returns the list of tokens as a string vector.*/
 vector<string> tokenize(const string& text);
-string Reverse(const string& text, const bool skipEndingNewline);
+
+/* Reverses the order of every character in a given text.
+Takes:
+- the text as a string.
+- a boolean determining whether or not to skip a newline that ends the text string.
+Returns the reversed text.*/
+string reverse(const string& text, const bool skipEndingNewline);
+
+/* Capitalizes the second letter of every word in the list.
+Take note:
+- Words with less than 2 characters are ignored.
+- Words where the second character is not capitalizable (e.g. non-letters) are not affected.
+
+Takes a list of words as a vector of strings.
+Returns a new list of words with each second character capitalized as a vector of strings.*/
 vector<string> capitalizeSecondLetters(const vector<string>& words);
 
 int main()
@@ -20,7 +59,7 @@ int main()
 	cout << "There are " << countVowels(fileData) << " vowels.\n";
 	cout << "There are " << countWords(fileData) << " words.\n";
 	cout << "Reversed text:\n";
-	cout << Reverse(fileData, true) << "\n";
+	cout << reverse(fileData, true) << "\n";
 	
 	cout << "Text with the 2nd letter in each word capitalized:\n";
 	vector<string> words = capitalizeSecondLetters(tokenize(fileData));
@@ -60,7 +99,7 @@ string readFile(const string& path)
 	return fileData;
 }
 
-int countVowels(const string& text)
+unsigned int countVowels(const string& text)
 {
 	unsigned int vowelNum = 0;
 	
@@ -105,7 +144,7 @@ vector<string> tokenize(const string& text)
 	return tokens;
 }
 
-string Reverse(const string& text, const bool skipEndingNewline)
+string reverse(const string& text, const bool skipEndingNewline)
 {
 	string reversedText = "";
 	
