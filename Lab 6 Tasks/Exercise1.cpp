15c2f5112c0b72dcee2f86d5213d7a5abbb19dd5
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-04-22 19:52:44 +0200

    Added prompting for and outputting of the int and string

--- Lab 6 Tasks/Exercise1.cpp
+++ Lab 6 Tasks/Exercise1.cpp
@@ -15,10 +15,45 @@ int main()
 		return 1;
 	}
 	
+	cout << "Enter an integer value:\n";
+	*dynamicInteger = inputInteger();
+	
+	cout << "Enter some text:\n";
+	getline(cin, *dynamicString);
+	
+	cout << endl;
+	
+	cout << "The integer entered is " << *dynamicInteger << "\n";
+	cout << "The text entered is \"" << *dynamicString << "\"\n";
+	
 	delete dynamicInteger;
 	delete dynamicString;
 	
 	cout << endl;
 	
 	return 0;
+}
+
+int inputInteger()
+{
+	int result;
+	bool success = false;
+	
+	do{
+		string line;
+		getline(cin, line);
+		try{
+			result = stoi(line);
+		}
+		catch (const exception& e)
+		{
+			cerr << "Cannot read \"" << line << "\" as an integer. Try again!\n";
+			continue;
+		}
+		
+		// only evaluates if stoi(line) is successful
+		success = true;
+	}while(!success);
+	
+	return result;
 }
