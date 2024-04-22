cffa940dbd739daf145bc1374f4e981acd6e13b0
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-04-22 19:45:59 +0200

    Added dynamically allocated varaibles
    Added declaration of dynamically allocated integer and string
    Added error check for if dynamic memory allocation fails
    Added proper deallocation of the dynamically allocated variables

--- Lab 6 Tasks/Exercise1.cpp
+++ Lab 6 Tasks/Exercise1.cpp
@@ -3,6 +3,20 @@ using namespace std;
 
 int main()
 {
+	int* dynamicInteger = new int;
+	string* dynamicString = new string;
+	
+	if(dynamicInteger == nullptr || dynamicString == nullptr)
+	{
+		cerr << "Fatal. Could not allocate sufficient memory.\n";
+		cerr << "Terminating...\n" << endl;
+		return 1;
+	}
+	
+	delete dynamicInteger;
+	delete dynamicString;
+	
+	cout << endl;
 	
 	return 0;
 }
