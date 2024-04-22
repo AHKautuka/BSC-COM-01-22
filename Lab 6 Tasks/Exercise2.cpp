889631b2776ea7e8c9a42250fde38a4ab3de4c03
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-04-22 19:56:22 +0200

    Added inputInteger function
    Abstracts the inputting of integers and error handling of invalid inputs

--- Lab 6 Tasks/Exercise2.cpp
+++ Lab 6 Tasks/Exercise2.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 using namespace std;
 
+int inputInteger();
+
 int main()
 {
 	
 	return 0;
+}
+
+int inputInteger()
+{
+	int result;
+	bool success = false;
+	
+	do{
+		string line;
+		getline(cin, line);
+		try{
+			result = stoi(line);
+		}
+		catch (const exception& e)
+		{
+			cerr << "Cannot read \"" << line << "\" as an integer. Try again!\n";
+			continue;
+		}
+		
+		// only evaluates if stoi(line) is successful
+		success = true;
+	}while(!success);
+	
+	return result;
 }
