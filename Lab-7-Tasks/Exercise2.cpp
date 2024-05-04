c038eacf089b672550e0a10e8f247818cbcaec2b
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-05-04 17:31:33 +0200

    Add second recatngle
    - Includes inputting of length and width; this time using a constructor
    - Includes outputting of the the area similar to the first rectangle

--- Lab-7-Tasks/Exercise2.cpp
+++ Lab-7-Tasks/Exercise2.cpp
@@ -5,17 +5,25 @@ using namespace std;
 
 int main()
 {
-	Rectangle rectangle;
+	Rectangle rectangle1;
 	
-	cout << "Enter the length of the Rectangle (a nonnegative decimal number):\n";
+	cout << "Enter the length of the first Rectangle (a nonnegative decimal number):\n";
+	rectangle1.setLength(enterNonNegativeNumber());
+	
+	cout << "Enter the width of the first Rectangle (a nonnegative decimal number):\n";
+	rectangle1.setWidth(enterNonNegativeNumber());
+	
+	cout << "The first rectangle has an area of " << rectangle1.getArea() << ".\n";
+	
+	cout << "Enter the length of the second Rectangle (a nonnegative decimal number):\n";
 	float length = enterNonNegativeNumber();
-	rectangle.setLength(length);
 	
-	cout << "Enter the width of the Rectangle (a nonnegative decimal number):\n";
+	cout << "Enter the width of the second Rectangle (a nonnegative decimal number):\n";
 	float width = enterNonNegativeNumber();
-	rectangle.setWidth(width);
 	
-	cout << "The rectangle has an area of " << rectangle.getArea() << ".\n";
+	Rectangle rectangle2(length, width);
+	
+	cout << "The second rectangle has an area of " << rectangle2.getArea() << ".\n";
 	
 	cout << endl;
 	
