be35faec5bce2eb1c07479e3ed97c058ac76abfb
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-05-04 17:21:47 +0200

    Create Exercise2

---
+++ Lab-7-Tasks/Exercise2.cpp
@@ -0,0 +1,23 @@
+#include "UserInput.hpp"
+#include "Rectangle.hpp"
+
+using namespace std;
+
+int main()
+{
+	Rectangle rectangle;
+	
+	cout << "Enter the length of the Rectangle (a nonnegative decimal number):\n";
+	float length = enterNonNegativeNumber();
+	rectangle.setLength(length);
+	
+	cout << "Enter the width of the Rectangle (a nonnegative decimal number):\n";
+	float width = enterNonNegativeNumber();
+	rectangle.setWidth(width);
+	
+	cout << "The rectangle has an area of " << rectangle.getArea() << ".\n";
+	
+	cout << endl;
+	
+	return 0;
+}
