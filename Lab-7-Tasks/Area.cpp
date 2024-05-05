02013630d7b44df289b96e28dc67acbd4216e59b
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-05-05 13:22:32 +0200

    Fix scoping and function signature

--- Lab-7-Tasks/Area.cpp
+++ Lab-7-Tasks/Area.cpp
@@ -1,18 +1,18 @@
 #include "Area.hpp"
 using namespace shapes;
 
-static float calculateArea(const Square& square)
+float Area::calculateArea(const Square& square)
 {
 	const float sideLength = square.getSideLength();
 	return sideLength * sideLength;
 }
 
-static float calculateArea(const Triangle& triangle)
+float Area::calculateArea(const Triangle& triangle)
 {
 	return triangle.getBaseLength() * triangle.getHeight();
 }
 
-static float calculateArea(const Circle& circle)
+float Area::calculateArea(const Circle& circle)
 {
 	const float radius = circle.getRadius();
 	return (float) (PI * radius * radius);
