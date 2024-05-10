4658f95aa57bbf722867e29c36454c58a077f446
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-05-10 17:58:19 +0200

    Add conversion of Jane to a string and a float, and their printing

--- Lab-8/Task-2-to-4/main.cpp
+++ Lab-8/Task-2-to-4/main.cpp
@@ -34,5 +34,11 @@ int main () {
 	int johnAge = John;
 	cout << "John's Age" << johnAge << endl;
 	
+	string janeFirstName = Jane;
+	cout << "Jane's FirstName" << janeFirstName << endl;
+	
+	float janeWeight = Jane;
+	cout << "Jane's weight" << janeWeight << endl;
+	
 	return 0;
 }
