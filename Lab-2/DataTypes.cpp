ec6d334a6dc643e1d5983c7245335a8788df8ce5
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-05-16 18:40:24 +0200

    Add Lab 2

---
+++ Lab-2/DataTypes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Size of char : " << sizeof(char) << " bytes\n";
+	cout << "Size of int : " << sizeof(int) << " bytes\n";
+	cout << "Size of short int : " << sizeof(short int) << " bytes\n";
+	cout << "Size of long int : " << sizeof(long int) << " bytes\n";
+	cout << "Size of float : " << sizeof(float) << " bytes\n";
+	cout << "Size of double : " << sizeof(double) << " bytes\n";
+	cout << "Size of wchar_t : " << sizeof(wchar_t) << " bytes" << endl;
+	
+	return 0;
+}
---
+++ Lab-2/GlobalVariables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Global variable
+int g = 20;
+
+int main()
+{
+	int g = 10;
+	
+	cout << g;
+	
+	return 0;
+}
